f172766df0aee4c44ccda5bcfe531ad7e6338b69
Gino <gmarigioli@gmail.com>
2026-09-06 22:16:57 -0300

    ejercicio 4 completado

--- src/parte 1/ejercicios.ts
+++ src/parte 1/ejercicios.ts
@@ -58,8 +58,8 @@ export function obtenerMayoresDeEdad(alumnos: Alumno[]): Alumno[] {
 // Un alumno aprueba cuando su nota es mayor o igual a 6.
 // Devolver los alumnos aprobados.
 export function obtenerAprobados(alumnos: Alumno[]): Alumno[] {
-    // TODO
-    throw new Error("Implementar");
+    // COMPLETADO
+    return alumnos.filter(alumno => alumno.nota >= 6);
 }
 
 // -----------------------------------------------------------------------------
